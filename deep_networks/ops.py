a7b707881b3ce2f85806629b461c9cb4c06c2b6a
Shaform <shaform@gmail.com>
2017-11-03 22:05:13 +0800

    update lrelu

--- deep_networks/ops.py
+++ deep_networks/ops.py
@@ -5,6 +5,4 @@ import tensorflow as tf
 def lrelu(x, leak=0.2, name='lrelu'):
     """Leaky ReLU"""
     with tf.name_scope(name):
-        f1 = 0.5 * (1 + leak)
-        f2 = 0.5 * (1 - leak)
-        return f1 * x + f2 * tf.abs(x)
+        return tf.nn.relu(x) - leak * tf.nn.relu(-x)
